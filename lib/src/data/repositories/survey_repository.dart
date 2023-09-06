99c24a2cdbd59e0aa5e8bcd2ffd9e356ed07aa9d
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-09-06 08:41:58 +0200

    Add SurveyRepository and WHO5 Survery

---
+++ lib/src/data/repositories/survey_repository.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_health_app/src/data/repositories/surveys/who5.dart';
+
+import '../models/survery.dart';
+
+enum SurveyEnum { 
+  who5
+ }
+
+class SurveyRepository {
+
+  Future<Survey> getSurvey(SurveyEnum survey) async {    
+    switch (survey) {
+      case SurveyEnum.who5:
+        return Survey("WHO-5", "WHO-5 Well-being Index", who5);
+      default:
+        throw Exception("Survey not found");
+    }
+    
+    
+  }
+
+  Future<List<Survey>> getActiveSurveys() async {
+    return [
+      Survey("WHO-5", "WHO-5 Well-being Index", who5)
+    ];
+  }
+
+
+}
