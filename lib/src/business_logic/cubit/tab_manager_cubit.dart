447143c4d94954a3a5a5fba14574a8b576024e61
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-09-06 20:53:22 +0200

    Convert to TabManager to use Equatable

--- lib/src/business_logic/cubit/tab_manager_cubit.dart
+++ lib/src/business_logic/cubit/tab_manager_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
 
 part 'tab_manager_state.dart';
 
--- lib/src/business_logic/cubit/tab_manager_state.dart
+++ lib/src/business_logic/cubit/tab_manager_state.dart
@@ -1,9 +1,12 @@
 part of 'tab_manager_cubit.dart';
 
-@immutable
-sealed class TabManagerState {
+sealed class TabManagerState extends Equatable {
   final int selectedTab;
+
   const TabManagerState(this.selectedTab);
+
+  @override
+  List<Object> get props => [selectedTab];
 }
 
 final class TabManagerInitial extends TabManagerState {
