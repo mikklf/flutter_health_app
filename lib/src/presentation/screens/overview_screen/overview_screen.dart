ae2fc959039d1da145fba7455a5d5a962cd8082f
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-09-25 08:43:46 +0200

    test

--- lib/src/presentation/screens/overview_screen/overview_screen.dart
+++ lib/src/presentation/screens/overview_screen/overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:health/health.dart';
 
 class OverviewScreen extends StatelessWidget {
   const OverviewScreen({
@@ -12,8 +13,43 @@ class OverviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Overview Screen"),
+    return Center(
+      child: ElevatedButton(
+          onPressed: () async {
+            HealthFactory health =
+                HealthFactory(useHealthConnectIfAvailable: true);
+
+            var types = [
+              HealthDataType.STEPS,
+            ];
+
+            bool requested = await health.requestAuthorization(types);
+
+            var now = DateTime.now();
+
+            // fetch health data from the last 24 hours
+            List<HealthDataPoint> healthData =
+                await health.getHealthDataFromTypes(
+                    now.subtract(Duration(days: 1)), now, types);
+
+            // request permissions to write steps and blood glucose
+            types = [HealthDataType.STEPS];
+            var permissions = [
+              HealthDataAccess.READ_WRITE
+            ];
+            await health.requestAuthorization(types, permissions: permissions);
+
+            // write steps and blood glucose
+            bool success = await health.writeHealthData(
+                10, HealthDataType.STEPS, now, now);
+
+            // get the number of steps for today
+            var midnight = DateTime(now.year, now.month, now.day);
+            int? steps = await health.getTotalStepsInInterval(midnight, now);
+
+            print("Steps: $steps");
+          },
+          child: const Text("Press me")),
     );
   }
 }
