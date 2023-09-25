334a5e49da97831205de9862a0eff4cfdddb7465
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-09-25 14:48:09 +0200

    Health plugin test

--- lib/src/presentation/screens/overview_screen/overview_screen.dart
+++ lib/src/presentation/screens/overview_screen/overview_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'dart:math';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:community_charts_flutter/community_charts_flutter.dart'
     as charts;
+import 'package:health/health.dart';
 
 class OverviewScreen extends StatelessWidget {
   const OverviewScreen({
@@ -16,6 +19,49 @@ class OverviewScreen extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(16),
       children: [
+        ElevatedButton(
+            onPressed: () async {
+              
+              HealthFactory health;
+              
+              // Due to a bug using Health Connect on Andriod SDK 34+ is not possible
+              // See https://github.com/cph-cachet/flutter-plugins/issues/800
+              // We therefore use Health Connect on Android SDK 33 and below
+              // and use soon-to-be-deprecated Google Fit API on SDK 34 and above
+              if (Platform.isIOS) {
+                health = HealthFactory();
+              } else {
+                final deviceInfo = DeviceInfoPlugin();
+                final androidInfo = await deviceInfo.androidInfo;
+
+                if (androidInfo.version.sdkInt >= 34) {
+                  health = HealthFactory();
+                } else {
+                  health = HealthFactory(useHealthConnectIfAvailable: true);
+                }
+              }
+
+              var types = [
+                HealthDataType.STEPS,
+              ];
+
+              var permission = [HealthDataAccess.READ_WRITE];
+
+              bool requested = await health.requestAuthorization(types,
+                  permissions: permission);
+
+              var earlier = DateTime.now().subtract(Duration(minutes: 20));
+              var now = DateTime.now();
+
+              bool success = await health.writeHealthData(
+                  150, HealthDataType.STEPS, earlier, now);
+
+              var midnight = DateTime(now.year, now.month, now.day);
+              int? steps = await health.getTotalStepsInInterval(midnight, now);
+
+              print("Steps: $steps");
+            },
+            child: const Text("Press me for health data")),
         SizedBox(
             height: 250,
             child: Card(
@@ -27,15 +73,16 @@ class OverviewScreen extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("Steps", style: Theme.of(context).textTheme.titleLarge),
-                      Text("10320 steps today", style: Theme.of(context).textTheme.bodyLarge),
+                      Text("Steps",
+                          style: Theme.of(context).textTheme.titleLarge),
+                      Text("10320 steps today",
+                          style: Theme.of(context).textTheme.bodyLarge),
                       const Icon(Icons.directions_walk),
                     ],
                   ),
                   Expanded(
                     child: SimpleBarChart.withRandomData(),
                   ),
-                
                 ]),
               ),
             )),
@@ -50,15 +97,16 @@ class OverviewScreen extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("Heatbeat", style: Theme.of(context).textTheme.titleLarge),
-                      Text("87 bpm", style: Theme.of(context).textTheme.bodyLarge),
+                      Text("Heatbeat",
+                          style: Theme.of(context).textTheme.titleLarge),
+                      Text("87 bpm",
+                          style: Theme.of(context).textTheme.bodyLarge),
                       const Icon(Icons.heart_broken),
                     ],
                   ),
                   Expanded(
                     child: SimpleBarChart.withRandomData(),
                   ),
-                
                 ]),
               ),
             )),
@@ -73,15 +121,16 @@ class OverviewScreen extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("Weight", style: Theme.of(context).textTheme.titleLarge),
-                      Text("81 kg", style: Theme.of(context).textTheme.bodyLarge),
+                      Text("Weight",
+                          style: Theme.of(context).textTheme.titleLarge),
+                      Text("81 kg",
+                          style: Theme.of(context).textTheme.bodyLarge),
                       const Icon(Icons.scale),
                     ],
                   ),
                   Expanded(
                     child: SimpleBarChart.withRandomData(),
                   ),
-                
                 ]),
               ),
             )),
@@ -131,7 +180,6 @@ class SimpleBarChart extends StatelessWidget {
       animate: animate,
     );
   }
-
 }
 
 /// Demonstration data class.
