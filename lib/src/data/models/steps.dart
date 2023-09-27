bf424df801157d19fda0db74b422361924fe1eae
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-09-27 12:18:57 +0200

    Add steps model

---
+++ lib/src/data/models/steps.dart
@@ -0,0 +1,43 @@
+import 'package:equatable/equatable.dart';
+
+class Steps extends Equatable {
+  final int steps;
+  final DateTime date;
+  final int? id;
+
+  const Steps(
+      {required this.steps,
+      required this.date,
+      this.id});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'steps': steps,
+      'date': date.toString(),
+      'id': id,
+    };
+  }
+
+  factory Steps.fromMap(Map<String, dynamic> map) {
+    return Steps(
+      steps: map['steps'] as int,
+      date: DateTime.parse(map['date']),
+      id: map['id'] as int?
+    );
+  }
+
+  Steps copyWith({
+    int? steps,
+    DateTime? date,
+    int? id,
+  }) {
+    return Steps(
+      steps: steps ?? this.steps,
+      date: date ?? this.date,
+      id: id ?? this.id,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id];
+}
