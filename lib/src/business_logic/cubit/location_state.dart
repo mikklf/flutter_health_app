8420273e4510b2fa90993c5ea99cb8900d756266
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-10-09 13:24:20 +0200

    Add list of locations to location state

--- lib/src/business_logic/cubit/location_state.dart
+++ lib/src/business_logic/cubit/location_state.dart
@@ -1,8 +1,19 @@
 part of 'location_cubit.dart';
 
 final class LocationState extends Equatable {
-  const LocationState();
+  
+  final List<Location> locations;
+  
+  const LocationState(this.locations);
   
   @override
   List<Object> get props => [];
+
+  LocationState copyWith({
+    List<Location>? locations,
+  }) {
+    return LocationState(
+      locations ?? this.locations,
+    );
+  }
 }
