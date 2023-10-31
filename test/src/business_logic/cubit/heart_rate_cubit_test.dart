1b368f5044eca0baa6bb584bb085ac17bea20310
Mikkel Fuchs <4072916+mikklf@users.noreply.github.com>
2023-10-31 09:36:06 +0100

    Add heart rate tests for cubit and repository

---
+++ test/src/business_logic/cubit/heart_rate_cubit_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_health_app/src/business_logic/cubit/heart_rate_cubit.dart';
+import 'package:flutter_health_app/src/data/models/heart_rate.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_health_app/domain/interfaces/heart_rate_repository.dart';
+
+class MockHeartRateRepository extends Mock implements IHeartRateRepository {}
+
+void main() {
+  late MockHeartRateRepository mockHeartRateRepository;
+  late HeartRateCubit heartRateCubit;
+
+  setUp(() {
+    mockHeartRateRepository = MockHeartRateRepository();
+    heartRateCubit = HeartRateCubit(mockHeartRateRepository);
+  });
+
+  tearDown(() {
+    heartRateCubit.close();
+  });
+
+  group('Heart_rate_cubit', () {
+    test('initial state is HeartRateState()', () {
+      // Assert
+      expect(heartRateCubit.state, const HeartRateState());
+      expect(heartRateCubit.state.heartRateList.isEmpty, true);
+      expect(heartRateCubit.state.highestHeartRate, 0);
+      expect(heartRateCubit.state.lowestHeartRate, 0);
+    });
+
+    test('emits HeartRateState with rates for the day', () async {
+      // Arrange
+      final mockData = [
+        HeartRate(
+            beatsPerMinute: 70,
+            timestamp: DateTime.utc(1969, 7, 20, 20, 18, 04))
+      ];
+
+      when(() => mockHeartRateRepository.getHeartRatesInRange(any(), any()))
+          .thenAnswer((_) async => mockData);
+
+      // Act
+      await heartRateCubit.getHeartRatesForDay();
+
+      // Assert
+      expect(heartRateCubit.state.heartRateList.length, 1);
+      expect(heartRateCubit.state.heartRateList[0].beatsPerMinute, 70);
+      expect(heartRateCubit.state.heartRateList[0].timestamp,
+          DateTime.utc(1969, 7, 20, 20, 18, 04));
+      verify(() => mockHeartRateRepository.getHeartRatesInRange(any(), any()))
+          .called(1);
+    });
+  });
+}
---
+++ test/src/data/repositories/heart_rate_repository_test.dart
@@ -0,0 +1,105 @@
+import 'package:flutter_health_app/domain/interfaces/health_provider.dart';
+import 'package:flutter_health_app/domain/interfaces/heart_rate_provider.dart';
+import 'package:flutter_health_app/src/data/models/heart_rate.dart';
+import 'package:flutter_health_app/src/data/repositories/heart_rate_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:health/health.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockHeartRateProvider extends Mock implements IHeartRateProvider {}
+
+class MockHealthProvider extends Mock implements IHealthProvider {}
+
+void main() {
+  group('HeartRateRepository', () {
+    final mockHeartRateProvider = MockHeartRateProvider();
+    final mockHealthProvider = MockHealthProvider();
+    final heartRateRepository =
+        HeartRateRepository(mockHeartRateProvider, mockHealthProvider);
+
+    test('getHeartRatesInRange returns empty list when provider returns null',
+        () async {
+      // Arrange
+      when(() => mockHeartRateProvider.getHeartRatesInRange(any(), any()))
+          .thenAnswer((_) async => null);
+
+      // Act
+      final result = await heartRateRepository.getHeartRatesInRange(
+          DateTime(2021, 1, 1), DateTime(2021, 1, 2));
+
+      // Assert
+      expect(result, isEmpty);
+    });
+
+    test('getHeartRatesInRange returns list of HeartRate objects', () async {
+      // Arrange
+      final heartRateMap = {'beats_per_minute': 80, 'timestamp': DateTime.now().toString()};
+      when(() => mockHeartRateProvider.getHeartRatesInRange(any(), any())).thenAnswer(
+          (_) async => [heartRateMap]);
+
+      // Act
+      final result = await heartRateRepository.getHeartRatesInRange(
+          DateTime(2021, 1, 1), DateTime(2021, 1, 2));
+
+      // Assert
+      expect(result, isNotEmpty);
+      expect(result.first.beatsPerMinute, heartRateMap['beats_per_minute']);
+      expect(result.first.timestamp, DateTime.parse(heartRateMap['timestamp'].toString()));
+    });
+
+    test('insert calls insert on heart rate provider', () async {
+      // Arrange
+      final heartRate = HeartRate(beatsPerMinute: 80, timestamp: DateTime.now());
+
+      when(() => mockHeartRateProvider.insert(any()))
+          .thenAnswer((_) async => {});
+
+      // Act
+      await heartRateRepository.insert(heartRate);
+
+      // Assert
+      verify(() => mockHeartRateProvider.insert(heartRate.toMap())).called(1);
+    });
+
+    test('syncHeartRates does nothing when health provider returns empty list',
+        () async {
+      // Arrange
+      when(() => mockHealthProvider.getHeartbeats(any(), any()))
+          .thenAnswer((_) async => []);
+      when(() => mockHeartRateProvider.insert(any()))
+          .thenAnswer((_) async => {});
+
+      // Act
+      await heartRateRepository.syncHeartRates(DateTime(2021, 1, 1));
+
+      // Assert
+      verifyNever(() => mockHeartRateProvider.insert(any()));
+    });
+
+    test('syncHeartRates inserts HeartRate objects when health provider returns data',
+        () async {
+      // Arrange
+      final healthDataPoint = HealthDataPoint(
+          NumericHealthValue(80),
+          HealthDataType.HEART_RATE,
+          HealthDataUnit.BEATS_PER_MINUTE,
+          DateTime(2021, 1, 1),
+          DateTime(2021, 1, 1),
+          PlatformType.ANDROID,
+          'device_id',
+          'source_id',
+          'source_name');
+
+      when(() => mockHealthProvider.getHeartbeats(any(), any()))
+          .thenAnswer((_) async => [healthDataPoint]);
+      when(() => mockHeartRateProvider.insert(any()))
+          .thenAnswer((_) async => {});
+
+      // Act
+      await heartRateRepository.syncHeartRates(DateTime(2021, 1, 1));
+
+      // Assert
+      verify(() => mockHeartRateProvider.insert(any())).called(1);
+    });
+  });
+}
